646fd236c7ae8c6ee927cc764d99405d0d89e109
Balu <barkoczybalu@gmail.com>
2023-10-28 22:24:07 +0200

    Disable nav, when user is logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,24 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { LoginGuard } from './guards/login.guard';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { 
     path: 'home', 
     component: HomeComponent,
-    canActivate: [LoginGuard]
+    canActivate: [AuthGuard]
   },
-  { path: 'register', component: RegisterComponent },
-  { path: '', component: LoginComponent },
+  { 
+    path: 'register', 
+    component: RegisterComponent,
+    canActivate: [LoginGuard]
+   },
+  { 
+    path: '', 
+    component: LoginComponent,
+    canActivate: [LoginGuard] 
+  },
   { path: '**', redirectTo: '' },
 ];
 
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -6,14 +6,13 @@ import { AuthService } from '../services/auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class LoginGuard implements CanActivate {
-  
+export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (!this.authService.user) return true;
+    if (this.authService.user) return true;
       
     return false;
   }
